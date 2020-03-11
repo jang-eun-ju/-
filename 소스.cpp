9db0ac9aaee096fdf68486e2a67912c24a23e5ee
jang-eun-ju <61079958+jang-eun-ju@users.noreply.github.com>
2020-03-12 00:14:14 +0900

    백준 10250

--- 소스.cpp
+++ 소스.cpp
@@ -1,59 +1,25 @@
-
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-
 
 int main() {
-	double a, b, c, first, second, third;
-	double i = 0, k = 0;
-	for (i = 0;; i++) {
-		scanf("%lf %lf %lf", &a, &b, &c);
-		if (a == 0 && b == 0 && c == 0)
-			break;
-		if (a >= b) {
-			first = a;
-			if (first <= c) {
-				first = c;
-				second = a;
-				third = b;
-			}
-			else if (b >= c) {
-				second = b;
-				third = c;
-			}
-			else {
-				second = c;
-				third = b;
-			}
+	int count, a, b, c, dong, ho;
+	scanf("%d", &count);
+	for (int i = 0; i < count; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		
+		if (c % a == 0) {
+			ho = c / a - 1;
 		}
 		else {
-			first = b;
-			if (first <= c) {
-				first = c;
-				second = b;
-				third = a;
-			}
-			else if (a >= c) {
-				second = a;
-				third = c;
-			}
-			else {
-				second = c;
-				third = a;
-			}
+			ho = c / a;
 		}
-		if (pow(second, 2) + pow(third, 2) == pow(first, 2)) {
-			printf("right\n");
+		dong = c - a * ho;
+		if (ho < 9) {
+			printf("%d0%d\n", dong, ho + 1);
 		}
 		else {
-			printf("wrong\n");
+			printf("%d%d\n", dong, ho + 1);
 		}
 	}
-
 	return 0;
 }
-
-
